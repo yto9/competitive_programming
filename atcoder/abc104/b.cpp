bdb30607ab0d0c346e2c9123ce00e978a5d71011
yto9 <ytoku.pro@gmail.com>
2018-08-07 20:06:07 +0900

    modify border condition

--- atcoder/abc104/b.cpp
+++ atcoder/abc104/b.cpp
@@ -18,7 +18,7 @@ int main(int argc, char const *argv[])
         }
     }
     if (count != 1) ac = false;
-    if (idx < 2 || S.size() - 3 < idx) ac = false;
+    if (idx < 2 || S.size() - 2 < idx) ac = false;
     FOR(i, 1, S.size()) {
         if (i == idx) continue;
         else if(S[i] < 94) ac = false;
