eb66e74e5705b5490eeaae29ed8a1230ff051d12
yto9 <ytoku.pro@gmail.com>
2018-08-07 20:19:36 +0900

    improve code based on editorial

--- atcoder/abc104/b.cpp
+++ atcoder/abc104/b.cpp
@@ -6,24 +6,19 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    bool ac = true;
-    int count = 0; 
-    int idx = 0;
+    string res = "AC\n";
+    int cnt = 0; 
     string S; cin >> S;
-    if (S[0] != 'A') ac = false;
-    REP(i, S.size()) {
-        if (S[i] == 'C') {
-            count++;
-            idx = i;
+    if (S[0] != 'A') res = "WA\n";
+    FOR(i, 1, S.size()){
+        if (isupper(S[i])){
+            if (i == 1 || i == S.size() - 1 || S[i] != 'C'){
+                res = "WA\n";
+            }
+            cnt++;
         }
     }
-    if (count != 1) ac = false;
-    if (idx < 2 || S.size() - 2 < idx) ac = false;
-    FOR(i, 1, S.size()) {
-        if (i == idx) continue;
-        else if(S[i] < 94) ac = false;
-    }
-    if (ac) cout << "AC\n";
-    else cout << "WA\n";
+    if (cnt != 1) res = "WA\n";
+    cout << res;
     return 0;
 }
