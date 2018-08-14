a60302ddc9c541447d86a959fb7ea4718ab45e2b
yto9 <ytoku.pro@gmail.com>
2018-08-14 13:09:17 +0900

    ABC007

---
+++ atcoder/ABC007/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FOR(i, a, b) for (int i = (a); i < (b); i++) 
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+int main(int argc, char const *argv[])
+{
+    int n; cin >> n;
+    cout << n - 1 << endl;
+    return 0;
+}
