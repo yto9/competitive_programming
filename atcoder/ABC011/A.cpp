9e02364e473d004613302db7c49554db270062c6
yto9 <ytoku.pro@gmail.com>
2018-08-16 14:20:19 +0900

    ABC011

---
+++ atcoder/ABC011/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FOR(i, a, b) for (int i = (a); i < (b); i++) 
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+int main(int argc, char const *argv[])
+{
+    int N; cin >> N;
+    cout << ((N != 12) ? N + 1: 1) << endl;
+    return 0;
+}
