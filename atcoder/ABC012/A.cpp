7455f67cc6e8a735e31855b42ff20db1a94a2e2d
yto9 <ytoku.pro@gmail.com>
2018-08-16 15:08:32 +0900

    ABC012

---
+++ atcoder/ABC012/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FOR(i, a, b) for (int i = (a); i < (b); i++) 
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+int main(int argc, char const *argv[])
+{
+    int A, B; cin >> A >> B;
+    cout << B << " " << A;
+    return 0;
+}
