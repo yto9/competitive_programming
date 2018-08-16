b14b54bfdbd6f681e3eaf3bb994a888dcbcbc0a0
yto9 <ytoku.pro@gmail.com>
2018-08-16 15:40:15 +0900

    ABC013

---
+++ atcoder/ABC013/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FOR(i, a, b) for (int i = (a); i < (b); i++) 
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+int main(int argc, char const *argv[])
+{
+    char X; cin >> X;
+    cout << X - 'A' + 1 << endl;
+    return 0;
+}
