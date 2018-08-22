11aa7f66c323c816f065cec5416f96871e769d22
yto9 <ytoku.pro@gmail.com>
2018-08-23 00:52:18 +0900

    ABC021

--- temp.cpp
+++ temp.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define FOR(i, a, b) for (int i = (a); i < (b); i++) 
 #define REP(i, n) for (int i = 0; i < (n); i++)
 
+typedef long long ll;
+
 int main(int argc, char const *argv[])
 {
     return 0;
